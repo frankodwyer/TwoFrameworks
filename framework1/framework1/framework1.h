57d5df77d233c9065d66bee6a8b6a487c79095df
Frank O'Dwyer <frankodwyer@netscape.net>
2019-10-18 11:20:35 +0100

    Initial commit

---
+++ framework1/framework1/framework1.h
@@ -0,0 +1,19 @@
+//
+//  framework1.h
+//  framework1
+//
+//  Created by Frank on 17/10/2019.
+//  Copyright © 2019 Frank O'Dwyer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for framework1.
+FOUNDATION_EXPORT double framework1VersionNumber;
+
+//! Project version string for framework1.
+FOUNDATION_EXPORT const unsigned char framework1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <framework1/PublicHeader.h>
+
+
---
+++ framework1/framework2/framework2/framework2.h
@@ -0,0 +1,19 @@
+//
+//  framework2.h
+//  framework2
+//
+//  Created by Frank on 17/10/2019.
+//  Copyright © 2019 Frank O'Dwyer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for framework2.
+FOUNDATION_EXPORT double framework2VersionNumber;
+
+//! Project version string for framework2.
+FOUNDATION_EXPORT const unsigned char framework2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <framework2/PublicHeader.h>
+
+
